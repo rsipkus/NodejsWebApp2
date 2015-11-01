90383e2a5558f22f96203e89ef35a57d4af68546
rsipkus <sipkus@gmail.com>
2015-11-01 23:59:34 +0000

    3

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-﻿var app = require('express')();
-var http = require('http').Server(app);
+﻿//var app = require('express');
+var http = require('http');
 
-
+/*
 app.get('/', function (req, res) {
     res.send('<h1>Hello world</h1>this is page one!');
 });
 
-http.listen(80,console.log("text"));
-/*
+http.listen(80,console.log("text"));*/
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World from the git hub baby2!\n');
-}).listen(port);*/
+}).listen(88);
