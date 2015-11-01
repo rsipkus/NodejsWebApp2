54ae1c1f193344c1554da52005b068ad20eada20
rsipkus <sipkus@gmail.com>
2015-11-01 23:54:21 +0000

    3

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 ﻿var app = require('express')();
 var http = require('http').Server(app);
-var port = process.env.port || 1337;
 
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello world</h1>this is page one!');
 });
 
-http.listen(80);
+http.listen(80,console.log("text"));
 /*
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
