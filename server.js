39969b157d49ca362879986a12b8327190713b30
rsipkus <sipkus@gmail.com>
2015-11-02 00:02:44 +0000

    5

--- server.js
+++ server.js
@@ -11,4 +11,9 @@ http.listen(80,console.log("text"));*/
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World from the git hub baby2!\n');
-}).listen(88);
+}).listen(80);
+
+
+function PerformATask(time, callback) {
+    setTimeout(callback, time);
+}
