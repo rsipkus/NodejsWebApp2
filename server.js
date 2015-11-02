e33b7466427d6fb4264c9394cfbecf04481409bc
rsipkus <sipkus@gmail.com>
2015-11-02 21:56:59 +0000

    express

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-﻿
-var http = require('http');
-var port = process.env.PORT || 3000;
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var port = process.env.PORT || 80;
 
-http.createServer(function (req, res) {
+app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World from the git hub baby2!\n');
-}).listen(port);
+    res.end('Hello World from the git hub baby express!\n');
+});
+
+http.listen(port, function () {
+    console.log('listening on *:80');
+});
+
 
 
